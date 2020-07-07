741609b280a10e15be586ed856413deb5de066c7
Eri Schon <erischon@gmail.com>
2020-07-07 14:16:17 +0200

    Modif 05

--- exo_05.rb
+++ exo_05.rb
@@ -1,15 +1,20 @@
+# #{} est une expression de substitution en 2 ""
+
 puts "On va compter le nombre d'heures de travail à THP"
+# calcul le temps de travail en heures
 puts "Travail : #{10 * 5 * 11}"
+# calcul le temps de travail en minutes
 puts "En minutes ça fait : #{10 * 5 * 11 * 60}"
 
 puts "Et en secondes ?"
-
+# calcul le temps de travail en secondes
 puts 10 * 5 * 11 * 60 * 60
 
 puts "Est-ce que c'est vrai que 3 + 2 < 5 - 7 ?"
-
+# calcul le temps de travail en minutes
 puts 3 + 2 < 5 - 7
 
+# calcul
 puts "Ça fait combien 3 + 2 ? #{3 + 2}"
 puts "Ça fait combien 5 - 7 ? #{5 - 7}"
 
